2df7425acb8264451efab858e591584611f8d9d3
nvavinash <avinashmk42@gmail.com>
2022-07-03 14:43:32 +0530

    Updates

---
+++ precache-manifest.ad92b30404c816bc67620b2ee072966c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f976f085c0e898f3750c",
+    "url": "/Texting/static/css/main.06b234b8.chunk.css"
+  },
+  {
+    "revision": "f976f085c0e898f3750c",
+    "url": "/Texting/static/js/main.1a4d07d2.chunk.js"
+  },
+  {
+    "revision": "88dee445c78ded11cb31",
+    "url": "/Texting/static/js/runtime~main.7f18c951.js"
+  },
+  {
+    "revision": "d7552a1f1f052f900238",
+    "url": "/Texting/static/js/2.265c2a91.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/Texting/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "c54273794f7a795158c72e8ba935bd53",
+    "url": "/Texting/index.html"
+  }
+];
